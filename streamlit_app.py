2a0a81f42445dd0af9466a7bf561e8b0fa0f4b9a
Bikram Chatterjee <bikram@konnexions.ch>
2023-11-13 13:28:20 +0100

    remove display hack

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,9 @@ with left:
     df = pd.DataFrame({"col1": [1, 2, 3, 4], "col2": ["a", "b", "c", "d"]})
     dynamic_filters = DynamicFilters(df, filters=["col1", "col2"])
     dynamic_filters.display_filters(location="columns", num_columns=2, gap="large")
-    dynamic_filters.display_df(use_container_width=True, hide_index=True)
+    dynamic_filters.display_df()
 with right:
     df = pd.DataFrame({"col3": [11, 12, 13, 14], "col4": ["aa", "bb", "cc", "dd"]})
     dynamic_filters = DynamicFilters(df, filters=["col3", "col4"])
     dynamic_filters.display_filters(location="columns", num_columns=2, gap="large")
-    dynamic_filters.display_df(use_container_width=True, hide_index=True)
+    dynamic_filters.display_df()
